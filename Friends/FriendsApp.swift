f09f975b7a220da26d904744326fe9c213f215b2
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-08-06 11:14:54 -0400

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Kenji Dela Cruz on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
