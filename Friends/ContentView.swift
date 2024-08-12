09ef4fd1966d5e66ab5ff311f202f146910f7147
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-08-12 15:18:54 -0400

    added challenges

--- Friends/ContentView.swift
+++ Friends/ContentView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query let users: [User]
+    @Query(sort: [SortDescriptor(\User.name)]) let users: [User]
     @State var loading = false
     var body: some View {
+        Button("delete"){
+            users.forEach {
+                modelContext.delete($0)
+            }
+        }
         NavigationStack{
             if loading {
                 Spacer()
@@ -34,37 +39,14 @@ struct ContentView: View {
             if users.isEmpty {
                 loading = true
                 Task{
-//                    try await UserViewModel().fetchUsers(modelContext: modelContext)
-                    try await fetchUsers()
+                    try await UserViewModel().fetchUsers(modelContext: modelContext)
                 }
+                loading = false
             }
+            print(users.count)
         }
     }
     
-    func fetchUsers() async throws{
-//        try await UserViewModel().fetchUsers(modelContext: modelContext)
-//        try await Task.sleep(nanoseconds: 1 * UInt64(1e9))
-//        loading = false
-        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")
-        else {
-            print("Invalid URL")
-            return
-        }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            let decodedResponse = try decoder.decode([User].self, from: data)
-            decodedResponse.forEach { modelContext.insert($0)}
-            try await Task.sleep(nanoseconds: 1 * UInt64(1e9))
-            loading = false
-            
-        } catch {
-            print(error)
-        }
-        
-        
-    }
 }
 
 #Preview {
--- Friends/UserViewModel.swift
+++ Friends/UserViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 import SwiftData
 
+@MainActor
 struct UserViewModel {
     
     func fetchUsers(modelContext: ModelContext) async throws{
